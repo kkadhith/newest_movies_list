ed72b3a9aa3c4599a2d2a50f795aa5169504551f
unknown <24882134+kkadhith@users.noreply.github.com>
2022-09-15 16:46:54 -0700

    added trending.dart

---
+++ lib/widgets/trending.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TrendingMovies extends StatelessWidget {
+  const TrendingMovies({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
